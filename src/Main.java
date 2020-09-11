b809fcc2ec4efb3466e47eef869056fd264e3836
Jasmine801 <emil.mamedov7779@gmail.com>
2020-09-11 11:59:52 +0300

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,53 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {}
-    private int rad;
-    private int dia;
-    private int length;
-    public Main(int a, int b, int c){
-        rad = a;
-        dia = b;
-        length = c;
+    public static int Fact(int a){
+        int f = 1; int b = 2;
+        do{
+            f *= b;
+            b++;
+        } while(b != a + 1);
+        return f;
     }
-    public void setrad(int a){
-        rad = a;
-    }
-    public void setdia(int b){
-        dia = b;
-    }
-    public void setlen(int c){
-        length = c;
-    }
-    public int getrad(){
-        return rad;
-    }
-    public int getdia(){
-        return dia;
-    }
-    public int getlen(){
-        return length;
+
+    public static  void main(String[] args) {
+        int [] arr = new int [] {1, 2, 3, 4, 5};
+        int S = 0;
+        for(int num : arr){
+            S = S + num;
+        }
+        System.out.println( "Сумма эл. массива через for: " + S);
+        int i = 0;
+        int Sum = 0;
+        while(i < 5){
+            Sum = arr[i] + Sum;
+            i++;
+        }
+        System.out.println("Сумма эл. массива через while: " + Sum);
+        int b = 0;
+        int Sum2 = 0;
+        do{
+            Sum2 = Sum2 + arr[b];
+            b++;
+        } while(b < 5);
+        System.out.println("Сумма эл. массива через do while: "+ Sum2);
+        int [] Array = new int [5];
+        for(int j = 0; j < Array.length; j++){
+            Array[j] = ((int)(Math.random() * 5));
+        }
+        System.out.println("Массив чисел созданный через метод random: " + Arrays.toString(Array));
+        Arrays.sort(Array);
+        System.out.println("Массив чисел созданный через метод random после сортировки: " + Arrays.toString(Array));
+        Random rand = new Random();
+        int [] Arr = new int[6];
+        for(int j = 0; j < Arr.length; j++) {
+            Arr[j] = rand.nextInt(10);
+        }
+        System.out.println("Массив чисел созданный через класс Random: " + Arrays.toString(Arr));
+        Arrays.sort(Arr);
+        System.out.println("Массив чисел созданный через класс Random после сортировки: " + Arrays.toString(Arr));
+        System.out.println("4! = " + Fact(4));
     }
 
 }
